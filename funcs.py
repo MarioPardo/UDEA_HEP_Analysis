db758c150fd95eab4d247b8c6bcc59cc77666546
Mario Pardo <mario.pardo003@gmail.com>
2024-07-03 12:51:28 -0500

    Using coffea, little bit of cleanup

--- funcs.py
+++ funcs.py
@@ -1,10 +1,27 @@
 import numpy as np
+import matplotlib as plt
+
+
+
 
 def calculateSignificance(s,b):
     return s/np.sqrt(s + b)
 
+
+
+
 def calculateWeight(num_events, effective_area, lumi = 300):
     return effective_area * lumi / num_events
 
+
 def applyWeight(data, weight):
     return data * weight;
+
+
+
+
+
+#### Plotting Functions
+
+
+
