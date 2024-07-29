cc056296173a3d9849a7125058692021ef3d0c20
Mario Pardo <mario.pardo003@gmail.com>
2024-07-29 14:16:31 -0500

    **

--- funcs.py
+++ funcs.py
@@ -9,12 +9,12 @@ def calculateSignificance(s,b):
 
 
 
-def calculateWeight(num_events, effective_area, lumi = 300):
+def calculateWeight(num_events, effective_area, lumi):
     return effective_area * lumi / num_events
 
 
 def getWeightAsArray(weight, numElems):
-    return np.full(num_elems, weight)
+    return np.full(numElems, weight)
 
 def applyWeight(data, weight):
     return data * weight;
